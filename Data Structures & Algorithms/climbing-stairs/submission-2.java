66765a0130a54013a297f9c23f7be0a7db791fb6
srinivas-3164 <125180399+srinivas-3164@users.noreply.github.com>
2026-06-23 18:03:29 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if (n == 1)
+            return 1;
+        int prev = 1;
+        int curr = 2;
+        int temp;
+        for (int i = 3; i <= n; i++) {
+            temp = prev;
+            prev = curr;
+            curr += temp; 
+        }
+
+        return curr;
+    }
+}
